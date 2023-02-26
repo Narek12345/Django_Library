55aa7753e122f8ea2bf7889f683fbc7f1ac6e237
Narek12345 <narekbayanduryan16@gmail.com>
2023-02-26 19:37:15 +0300

    Added models to add: books, songs, photos and videos

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import AddBook, AddSong, AddPhoto, AddVideo
 
-# Register your models here.
+
+# Добавление модели AddBook.
+admin.site.register(AddBook)
+
+# Добавление модели AddSong.
+admin.site.register(AddSong)
+
+# Добавление модели AddPhoto.
+admin.site.register(AddPhoto)
+
+# Добавления модели AddVideo.
+admin.site.register(AddVideo)
--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AddBook(models.Model):
+	"""Модель для добавления книг."""
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=150)
+	author = models.CharField(max_length=50, blank=True)
+	book = models.FileField(upload_to="books/%Y/%m/%d/")
+	publication_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		"""Возвращает строковое представление модели."""
+		return self.title
+
+
+class AddSong(models.Model):
+	"""Модель для добавления песен."""
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=100)
+	author = models.CharField(max_length=50, blank=True)
+	song = models.FileField(upload_to="songs/%Y/%m/%d/")
+	publication_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		"""Возвращает строковое представление модели."""
+		return self.title
+
+
+class AddPhoto(models.Model):
+	"""Модель для добавления фотографий."""
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=150)
+	photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+	publication_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		"""Возвращает строковое представление модели."""
+		return self.title
+
+
+class AddVideo(models.Model):
+	"""Модель для добавления фотографий."""
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	title = models.CharField(max_length=150)
+	video = models.FileField(upload_to="videos/%Y/%m/%d/")
+	publication_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		"""Возвращает строковое представление модели."""
+		return self.title
