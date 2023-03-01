a7ef3ad368e7292c408dc1a98e852e8022c924b6
Narek12345 <narekbayanduryan16@gmail.com>
2023-03-01 20:59:11 +0300

    Created views for each table from the model and for each specific topic from the model.

--- library_app/urls.py
+++ library_app/urls.py
@@ -16,4 +16,20 @@ urlpatterns = [
 	path('new_photo/', views.new_photo, name="new_photo"),
 	# Страница для добавления видео.
 	path('new_video/', views.new_video, name="new_video"),
+	# Страница для просмотра всех книг.
+	path('view_all_books/', views.view_all_books, name="view_all_books"),
+	# Страница для просмотра всех песен.
+	path('view_all_songs/', views.view_all_songs, name="view_all_songs"),
+	# Страница для просмотра всех фото.
+	path('view_all_photos', views.view_all_photos, name="view_all_photos"),
+	# Страница для просмотра всех видео.
+	path('view_all_videos/', views.view_all_videos, name="view_all_videos"),
+	# Страница для просмотра конкретной книги.
+	path('view_book/<int:book_id>', views.view_book, name="view_book"),
+	# Страница для просмотра конкретной песни.
+	path('view_song/<int:song_id>', views.view_song, name="view_song"),
+	# Страница для просмотра конкретного фото.
+	path('view_photo/<int:photo_id>', views.view_photo, name="view_photo"),
+	# Страница для просмотра конкретного видео.
+	path('view_video/<int:video_id>', views.view_video, name="view_video"),
 ]
--- library_app/views.py
+++ library_app/views.py
@@ -1,5 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.http import Http404
 from datetime import datetime
+
+from .models import (
+	AddBook,
+	AddSong,
+	AddPhoto,
+	AddVideo
+)
 from .forms import (
 	AddBookForm,
 	AddSongForm,
@@ -12,7 +21,7 @@ def index(request):
 	"""Домашняя страница."""
 	return render(request, 'library_app/index.html')
 
-
+@login_required
 def new_book(request):
 	"""Добавляет новую книгу."""
 	if request.method != 'POST':
@@ -32,6 +41,7 @@ def new_book(request):
 	return render(request, 'library_app/new_book.html', context)
 
 
+@login_required
 def new_song(request):
 	"""Добавляет новую песню."""
 	if request.method != 'POST':
@@ -51,6 +61,7 @@ def new_song(request):
 	return render(request, 'library_app/new_song.html', context)
 
 
+@login_required
 def new_photo(request):
 	"""Добавляет новое фото."""
 	if request.method != 'POST':
@@ -70,6 +81,7 @@ def new_photo(request):
 	return render(request, 'library_app/new_photo.html', context)
 
 
+@login_required
 def new_video(request):
 	"""Добавляет новое видео."""
 	if request.method != 'POST':
@@ -87,3 +99,103 @@ def new_video(request):
 	# Вывести пустую или недействительную форму.
 	context = {'form': form}
 	return render(request, 'library_app/new_video.html', context)
+
+
+@login_required
+def view_all_books(request):
+	"""Просмотр добавленных книг."""
+	# Берем из БД все добавленные книги.
+	all_books = AddBook.objects.all()[::-1]
+	db_info = None
+	error_info = None
+
+	# Проверяем не пусто ли наше БД.
+	if len(all_books) != 0:
+		db_info = all_books
+	else:
+		error_info = "Вы еще не добавили данные в БД."
+
+	context = {'db_info': db_info, 'error_info': error_info}
+	return render(request, 'library_app/view_all_books.html', context)
+
+
+@login_required
+def view_all_songs(request):
+	"""Просмотр добавленных песен."""
+	# Берем из БД все добавленные книги.
+	all_songs = AddSong.objects.all()[::-1]
+	db_info = None
+	error_info = None
+
+	# Проверяем не пусто ли наше БД.
+	if len(all_songs) != 0:
+		db_info = all_songs
+	else:
+		error_info = "Вы еще не добавили данные в БД."
+
+	context = {'db_info': db_info, 'error_info': error_info}
+	return render(request, 'library_app/view_all_songs.html', context)
+
+
+@login_required
+def view_all_photos(request):
+	"""Просмотр добавленных песен."""
+	# Берем из БД все добавленные книги.
+	all_photos = AddPhoto.objects.all()[::-1]
+	db_info = None
+	error_info = None
+
+	# Проверяем не пусто ли наше БД.
+	if len(all_photos) != 0:
+		db_info = all_photos
+	else:
+		error_info = "Вы еще не добавили данные в БД."
+
+	context = {'db_info': db_info, 'error_info': error_info}
+	return render(request, 'library_app/view_all_photos.html', context)
+
+
+@login_required
+def view_all_videos(request):
+	"""Просмотр добавленных песен."""
+	# Берем из БД все добавленные книги.
+	all_videos = AddVideo.objects.all()[::-1]
+	db_info = None
+	error_info = None
+
+	# Проверяем не пусто ли наше БД.
+	if len(all_videos) != 0:
+		db_info = all_videos
+	else:
+		error_info = "Вы еще не добавили данные в БД."
+
+	context = {'db_info': db_info, 'error_info': error_info}
+	return render(request, 'library_app/view_all_videos.html', context)
+
+
+@login_required
+def view_book(request, book_id):
+	"""Просмотр конкретной книги."""
+	book = AddBook.objects.get(id=book_id)
+	return render(request, 'library_app/view_book.html', {'book': book})
+
+
+@login_required
+def view_song(request, song_id):
+	"""Просмотр конкретной песни."""
+	song = AddSong.objects.get(id=song_id)
+	return render(request, 'library_app/view_song.html', {'song': song})
+
+
+@login_required
+def view_photo(request, photo_id):
+	"""Просмотр конкретной книги."""
+	photo = AddPhoto.objects.get(id=photo_id)
+	return render(request, 'library_app/view_photo.html', {'photo': photo})
+
+
+@login_required
+def view_video(request, video_id):
+	"""Просмотр конкретного видео."""
+	video = AddVideo.objects.get(id=video_id)
+	return render(request, 'library_app/view_video.html', {'video': video})
